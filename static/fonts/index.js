332f7c2c72671a1394504ea4f7f1e4703a2f514b
Pidaus <firdaus.kamal251@gmail.com>
2019-10-06 19:59:13 +0800

    News Feed: - Styling - Request data - Pagination on scroll

---
+++ static/fonts/index.js
@@ -0,0 +1,22 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faRocket,
+  faNewspaper,
+  faCalendarPlus,
+  faGlobe,
+  faLock,
+  faComment,
+  faCommentSlash,
+  faPaperclip
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faRocket,
+  faNewspaper,
+  faCalendarPlus,
+  faGlobe,
+  faLock,
+  faComment,
+  faCommentSlash,
+  faPaperclip
+);
---
+++ utils/config.js
@@ -0,0 +1,1 @@
+export const BASE_API = "https://staging.sync.my/api/v2";
